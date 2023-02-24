3100274c93e6d5e6abf38711053c44b65322b1bb
pengpengjed <27i-7sdwnhaxo@dingtalk.com>
2023-02-24 15:43:05 +0800

    上手

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div ></div>
+  <div class="app">
+    <!-- store中的counter -->
+    <h2>App当前计数: {{$store.state.counter}}</h2>
+
+    <!-- home页面 -->
+    <hr>
+    <home></home>
+  </div>
 </template>
 
 <script setup>
+import Home from './views/Home.vue'
 
 </script>
 <style scoped lang='less'>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h2>Home当前计数: {{$store.state.counter}}</h2>
+  </div>
+</template>
+
+<script setup>
+</script>
+<style scoped lang='less'>
+</style>
